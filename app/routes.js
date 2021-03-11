dd8d53fec4215d55c9ade9ed4123600c9c2f2e4e
Lewis Nyman <hello@lewisnyman.co.uk>
2021-03-11 20:52:45 +0000

    Add dynamic question routes

--- app/routes.js
+++ app/routes.js
@@ -1,6 +1,22 @@
 const express = require('express')
 const router = express.Router()
+const fs = require('fs')
 
 // Add your routes here - above the module.exports line
+
+const dataPath = './app/data/questions.json';
+let questions;
+
+fs.readFile(dataPath, 'utf8', (err, data) => {
+    if (err) {
+      throw err;
+    }
+    questions = JSON.parse(data);
+});
+
+router.get('/prototype-2/question/:questionID', (req, res) => {
+    const question = questions.find( ({ id }) => id === req.params.questionID );
+    res.render('prototype-2/question', { question: question })
+});
 
 module.exports = router
