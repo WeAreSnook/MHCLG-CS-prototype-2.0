4fa2f344c5e478d875c51902296fef2301588cd9
Lewis Nyman <hello@lewisnyman.co.uk>
2021-03-12 07:38:53 +0000

    Add data and render a section overview page

--- app/routes.js
+++ app/routes.js
@@ -4,19 +4,36 @@ const fs = require('fs')
 
 // Add your routes here - above the module.exports line
 
-const dataPath = './app/data/questions.json';
+const questionsPath = './app/data/questions.json';
 let questions;
+const sectionsPath = './app/data/sections.json';
+let sections;
 
-fs.readFile(dataPath, 'utf8', (err, data) => {
+fs.readFile(questionsPath, 'utf8', (err, data) => {
     if (err) {
       throw err;
     }
     questions = JSON.parse(data);
 });
+fs.readFile(sectionsPath, 'utf8', (err, data) => {
+    if (err) {
+      throw err;
+    }
+    sections = JSON.parse(data);
+    // Convert category CSVs into array
+    sections.forEach(section => {
+        section.categories = section.categories.split(',');
+    });
+    
+});
 
 router.get('/prototype-2/question/:questionID', (req, res) => {
     const question = questions.find( ({ id }) => id === req.params.questionID );
     res.render('prototype-2/question', { question: question })
+});
+
+router.get('/prototype-2/assessment-index/', (req, res) => {
+    res.render('prototype-2/assessment-index', { sections: sections })
 });
 
 module.exports = router
